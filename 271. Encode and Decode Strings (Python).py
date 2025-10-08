20d7743b51dbb6a49796b4aff9d99d316fa24dc1
Shiv P <shivbpat13@gmail.com>
2025-10-08 14:35:08 -0400

    Initial commit

--- 271. Encode and Decode Strings (Python).py
+++ 271. Encode and Decode Strings (Python).py
@@ -1,3 +1,5 @@
+from typing import List 
+
 class Solution:
     def encode(self, strs: List[str]) -> str:
         encoded_parts = []
--- 347. Top K Frequent Elements (Python).py
+++ 347. Top K Frequent Elements (Python).py
@@ -1,3 +1,6 @@
+from collections import Counter
+from typing import List
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counter = Counter(nums)
