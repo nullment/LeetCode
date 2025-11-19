6742230df1b01d3da540b68cda3515cacf0f3021
shiv <147013348+nullment@users.noreply.github.com>
2025-11-19 11:38:44 -0500

    Implement FizzBuzz solution in fizz_buzz.java

---
+++ fizz_buzz.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> output = new ArrayList<>();
+
+        for (int i = 1; i <= n; ++i){
+            if (i % 15 == 0) {
+                output.add("FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                output.add("Fizz");
+            }
+    
+            else if (i % 5 == 0) {
+
+                output.add("Buzz");
+            }
+            else {
+                output.add(Integer.toString(i));
+            }
+        }
+        return output;
+    }
+}
